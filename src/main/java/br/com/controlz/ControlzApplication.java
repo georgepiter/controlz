5039e852652fc2a85aa023488bb99d48302cc682
George Piter <68233141+georgepiter@users.noreply.github.com>
2022-12-26 19:47:43 -0300

    Add files via upload

---
+++ src/main/java/br/com/controlz/ControlzApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controlz/ControlzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
