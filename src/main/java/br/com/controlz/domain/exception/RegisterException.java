c4101768e339c4d0bda9872fb0eead6e79d3b0d7
georgepiter <george.piter@terra.com.br>
2022-12-29 21:26:03 -0300

    created handler for registerService

--- src/main/java/br/com/controlz/domain/exception/RegisterException.java
+++ src/main/java/br/com/controlz/domain/exception/RegisterException.java
@@ -1,6 +1,7 @@
 package br.com.controlz.domain.exception;
 
 public class RegisterException extends Exception {
+
 	public RegisterException(String message) {
 		super(message);
 	}
---
+++ src/main/java/br/com/controlz/domain/exceptionHandler/RestExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.controlz.domain.exceptionHandler;
+
+import br.com.controlz.domain.dto.ResponseEntityError;
+import br.com.controlz.domain.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({
+			PhoneException.class,
+			EmailException.class,
+			FieldException.class,
+			ValueException.class
+	})
+	public ResponseEntityError handleBadRequest(Exception e) {
+		return new ResponseEntityError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.CONFLICT)
+	@ExceptionHandler({
+			RegisterException.class
+	})
+	public ResponseEntityError handleConflict(Exception e) {
+		return new ResponseEntityError(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, e.getMessage());
+	}
+}
