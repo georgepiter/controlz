02c412f8c722121def803d8e75672700e3538f98
georgepiter <george.piter@terra.com.br>
2023-01-14 00:07:37 -0300

    implement debt email and adjust jobs

--- src/main/java/br/com/controlz/ControlzApplication.java
+++ src/main/java/br/com/controlz/ControlzApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+
 @SpringBootApplication
-//@EnableScheduling
 public class ControlzApplication {
 
 	public static void main(String[] args) {
