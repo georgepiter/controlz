129dafab85cc2e25ca103051113e1f739dd5b5b9
georgepiter <george.piter@terra.com.br>
2023-04-12 10:13:37 -0300

    update project, remove code smells

--- src/main/java/br/com/controlz/domain/repository/EmailPropertyRepository.java
+++ src/main/java/br/com/controlz/domain/repository/EmailPropertyRepository.java
@@ -4,8 +4,6 @@ import br.com.controlz.domain.entity.security.EmailProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface EmailPropertyRepository extends JpaRepository<EmailProperty, Long> {
 
