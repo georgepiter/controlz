c7a28d6380c8e425314aeb50830704bc6745461c
georgepiter <george.piter@terra.com.br>
2023-09-21 19:35:46 -0300

    update project, refactor cache category

--- src/main/java/br/com/controlz/controller/CategoryController.java
+++ src/main/java/br/com/controlz/controller/CategoryController.java
@@ -8,20 +8,15 @@ import br.com.controlz.domain.exception.CategoryNotFoundException;
 import br.com.controlz.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.Cacheable;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-@EnableCaching
 @RestController
 @Api(value = "Categoria", produces = MediaType.APPLICATION_JSON_VALUE, tags = {"Categoria"})
 @RequestMapping(value = "api/v1/category")
@@ -31,13 +26,13 @@ public class CategoryController {
 	private final CachedService cachedService;
 
 	public CategoryController(CategoryService categoryService,
-	                          CachedService cachedService) {
+	                          CachedService cachedService1) {
 		this.categoryService = categoryService;
-		this.cachedService = cachedService;
+		this.cachedService = cachedService1;
 	}
 
 	@PostMapping(value = "/")
-	@CacheEvict(value = "getCategory", allEntries = true)
+	@CacheEvict(value = "category", allEntries = true)
 	@ApiOperation(value = "Método que registra uma nova categoria")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
 	public ResponseEntityCustom registerNewCategory(@RequestBody CategoryDTO categoryDTO) throws CategoryNotFoundException {
@@ -45,42 +40,22 @@ public class CategoryController {
 	}
 
 	@GetMapping(value = "/all/{registerId}")
-	@Cacheable(value = "getCategory")
+	@Cacheable(value = "category")
 	@ApiOperation(value = "Método que retorna todas categorias por registerId")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
 	public List<CategoryDTO> getAllCategoriesByRegisterId(@PathVariable Long registerId) throws CategoryNotFoundException {
-		List<CategoryDTO> categoryDTOS;
-		String cachedName = "getCategory";
-		Optional<Cache.ValueWrapper> cacheValue = cachedService.getCachedById(cachedName, registerId);
-
-		if (cacheValue.isPresent()) {
-			categoryDTOS = new ArrayList<>((List<CategoryDTO>) cacheValue.get().get());
-		} else {
-			categoryDTOS = categoryService.getAllCategoriesByRegisterId(registerId);
-			cachedService.putCachedById(registerId, categoryDTOS, cachedName);
-		}
-		return categoryDTOS;
+		return cachedService.getOrLoadCategoriesFromCache(registerId);
 	}
 
 	@GetMapping(value = "/{categoryId}")
-	@Cacheable(value = "getCategoryId")
+	@Cacheable(value = "category")
 	@ApiOperation(value = "Método que retorna a categoria pelo id")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
 	public CategoryDTO getCategoryId(@PathVariable Long categoryId) throws CategoryNotFoundException {
-		CategoryDTO categoryDTO;
-		String cachedName = "getCategoryId";
-		Optional<Cache.ValueWrapper> cacheValue = cachedService.getCachedById(cachedName, categoryId);
-
-		if (cacheValue.isPresent()) {
-			categoryDTO = ((CategoryDTO) cacheValue.get().get());
-		} else {
-			categoryDTO = categoryService.getCategoryId(categoryId);
-			cachedService.putCachedById(categoryId, categoryDTO, cachedName);
-		}
-		return categoryDTO;
+		return cachedService.getOrLoadCategoryFromCache(categoryId);
 	}
 
-	@CachePut(value = "getCategory")
+	@CachePut(value = "category")
 	@PutMapping(value = "/update")
 	@ApiOperation(value = "Método que atualiza a categoria")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
@@ -91,7 +66,7 @@ public class CategoryController {
 	@DeleteMapping(value = "/{categoryId}/{registerId}")
 	@ApiOperation(value = "Método que deleta uma categoria por Id")
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
-	@CacheEvict(value = "getCategory", allEntries = true)
+	@CacheEvict(value = "category", allEntries = true)
 	public ResponseEntityCustom deleteCategoryById(@PathVariable Long categoryId, @PathVariable Long registerId) throws CategoryNotFoundException, CategoryDeleteException {
 		return categoryService.deleteCategoryById(categoryId, registerId);
 	}
--- src/main/java/br/com/controlz/domain/cached/CachedService.java
+++ src/main/java/br/com/controlz/domain/cached/CachedService.java
@@ -1,9 +1,14 @@
 package br.com.controlz.domain.cached;
 
+import br.com.controlz.domain.dto.CategoryDTO;
+import br.com.controlz.domain.exception.CategoryNotFoundException;
+import br.com.controlz.service.CategoryService;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -11,22 +16,53 @@ import java.util.Optional;
 public class CachedService {
 
 	private final CacheManager cacheManager;
+	private final CategoryService categoryService;
 
-	public CachedService(CacheManager cacheManager) {
+	public CachedService(CacheManager cacheManager,
+	                     CategoryService categoryService) {
 		this.cacheManager = cacheManager;
+		this.categoryService = categoryService;
 	}
 
-	public Optional<Cache.ValueWrapper> getCachedById(String cacheName, long id) {
+	protected Optional<Cache.ValueWrapper> getCachedById(String cacheName, long id) {
 		Cache cache = cacheManager.getCache(cacheName);
 		Cache.ValueWrapper valueWrapper = Objects.isNull(cache.get(id)) ? null : cache.get(id);
 		return Optional.ofNullable(valueWrapper);
 	}
 
-	public void putCachedById(long id, Object value, String cachedName) {
+	protected void putCachedById(long id, Object value, String cachedName) {
 		Cache cache = cacheManager.getCache(cachedName);
 		if (!Objects.isNull(cache)) {
 			cache.put(id, value);
 		}
+	}
+
+	public List<CategoryDTO> getOrLoadCategoriesFromCache(Long registerId) throws CategoryNotFoundException {
+		List<CategoryDTO> categoryDTOS;
+		String cachedName = "category";
+		Optional<Cache.ValueWrapper> cacheValue = getCachedById(cachedName, registerId);
+
+		if (cacheValue.isPresent()) {
+			categoryDTOS = new ArrayList<>((List<CategoryDTO>) cacheValue.get().get());
+		} else {
+			categoryDTOS = categoryService.getAllCategoriesByRegisterId(registerId);
+			putCachedById(registerId, categoryDTOS, cachedName);
+		}
+		return categoryDTOS;
+	}
+
+	public CategoryDTO getOrLoadCategoryFromCache(Long categoryId) throws CategoryNotFoundException {
+		CategoryDTO categoryDTO;
+		String cachedName = "category";
+		Optional<Cache.ValueWrapper> cacheValue = getCachedById(cachedName, categoryId);
+
+		if (cacheValue.isPresent()) {
+			categoryDTO = ((CategoryDTO) cacheValue.get().get());
+		} else {
+			categoryDTO = categoryService.getCategoryId(categoryId);
+			putCachedById(categoryId, categoryDTO, cachedName);
+		}
+		return categoryDTO;
 	}
 
 }
